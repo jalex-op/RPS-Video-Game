490e2f8d5f8ad40a08771ef9a24403ebbade0463
Jordan <148658758+jalex-op@users.noreply.github.com>
2023-11-01 14:45:19 -0230

    reworked code; now uses buttons to play game

--- rps.js
+++ rps.js
@@ -1,78 +1,85 @@
+let userScore = 0;
+let computerScore = 0;
+let roundsPlayed = 0;
+
+
+
+function updateDisplay(message, finalResult = false) {
+    const resultDiv = document.getElementById('result');
+    const scoreDiv = document.getElementById('score');
+    const finalResultDiv = document.getElementById('finalResult');
+  
+    if (finalResult) {
+        finalResultDiv.textContent = message;
+        resultDiv.textContent = '';
+    }
+    else {
+        resultDiv.textContent = 'Round ' + roundsPlayed + ': ' + message;
+    }
+
+    scoreDiv.textContent = 'Current Score - You: ' + userScore + ', Computer: ' + computerScore;
+  
+    if (userScore === 0 && computerScore === 0 && roundsPlayed === 0) {
+        finalResultDiv.textContent = '';
+    }
+  }
+
+
 function getComputerChoice(min, max) {
-    let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-    let computerChoice;
-    if (randomNumber === 1) {
-        computerChoice = 'Rock';
-    }
-    else if (randomNumber === 2) {
-        computerChoice = 'Paper';
-    }
-    else if (randomNumber === 3) {
-        computerChoice = 'Scissors';
-    }
-    return computerChoice.toLowerCase(); // Modification: convert to lowercase
+    const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    if (randomNumber === 1) return 'rock';
+    if (randomNumber === 2) return 'paper';
+    if (randomNumber === 3) return 'scissors';
 }
-
-// This section is unnecessary as you're getting the computer choice again in the playGame function
-// let computerChoice = getComputerChoice(1, 3);
-// console.log('AI picked:', computerChoice);
-
-function getUsrChoice() {
-    let usrChoice;
-    let validInput = false;
-    while (!validInput) {
-        usrChoice = prompt('Enter a choice: Rock, Paper, or Scissors. (The first person to  wins, wins the set!').toLowerCase();
-        if (usrChoice === 'rock' || usrChoice === 'paper' || usrChoice === 'scissors') {
-            validInput = true;
-        } else {
-            alert('Not a valid input. Check your spelling!')
-        }
-    }
-    return usrChoice;
-}
-
-// This section is unnecessary as you're getting the user choice again in the playGame function
-// const usrChoice = getUsrChoice();
-// console.log('User picked', usrChoice);
 
 function rps(computerChoice, usrChoice) {
-    let result;
     if (computerChoice === usrChoice) {
-        result = 'It\'s a tie! You both chose ' + usrChoice + '.';
+        return 'It\'s a tie! You both chose ' + usrChoice + '.';
     } else if (
         (computerChoice === 'rock' && usrChoice === 'scissors') ||
         (computerChoice === 'paper' && usrChoice === 'rock') ||
         (computerChoice === 'scissors' && usrChoice === 'paper')
     ) {
-        result = 'You lose! ' + usrChoice + ' is beaten by ' + computerChoice + '.';
+        computerScore++;
+        return 'You lose! ' + usrChoice + ' is beaten by ' + computerChoice + '.';
     } else {
-        result = 'You win! ' + usrChoice + ' beats ' + computerChoice + '.';
+        userScore++;
+        return 'You win! ' + usrChoice + ' beats ' + computerChoice + '.';
     }
-    alert('You picked: ' + usrChoice + '. Computer picked: ' + computerChoice + '. ' + result); 
-    return result; 
 }
 
-function playGame() {
-    let userScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        const computerChoice = getComputerChoice(1, 3);
-        const usrChoice = getUsrChoice();
-        const result = rps(computerChoice, usrChoice);
-        if (result === 'You win! ' + usrChoice + ' beats ' + computerChoice + '.') {
-            userScore++;
-        } else if (result === 'You lose! ' + usrChoice + ' is beaten by ' + computerChoice + '.') {
-            computerScore++;
+function checkGameOver() {
+    if (roundsPlayed === 5) {
+        let finalMessage;
+        if (userScore > computerScore) {
+            finalMessage = 'Congratulations! You won the best of 5 series ' + userScore + ' to ' + computerScore + '!';
+        } else if (userScore < computerScore) {
+            finalMessage = 'OH NO... you lost the best of 5 series ' + userScore + ' to ' + computerScore + '!';
+        } else {
+            finalMessage = 'The best of 5 series is a tie! ' + userScore + ' to ' + computerScore + '!';
         }
-        alert('Current Score - You: ' + userScore + ', Computer: ' + computerScore); 
-    }
-    if (userScore > computerScore) {
-        alert('You won the best of 5 series ' + userScore + ' to ' + computerScore + '!');
-    } else if (userScore < computerScore) {
-        alert('OH NO... you lost the best of 5 series ' + userScore + ' to ' + computerScore + '!');
-    } else {
-        alert('The best of 5 series is a tie! ' + userScore + ' to ' + computerScore + '!');
+        updateDisplay(finalMessage, true);
+        // Reset scores for a new series
+        userScore = 0;
+        computerScore = 0;
+        roundsPlayed = 0;
+        
     }
 }
 
-playGame();
+function playRound(userChoice) {
+    if (roundsPlayed === 0) {
+        const finalResultDiv = document.getElementById('finalResult');
+        finalResultDiv.textContent = '';
+    }
+    const computerChoice = getComputerChoice(1, 3);
+    const result = rps(computerChoice, userChoice);
+    roundsPlayed++;
+    updateDisplay(result);
+    checkGameOver();
+}
+
+document.getElementById('rock').addEventListener('click', () => playRound('rock'));
+document.getElementById('paper').addEventListener('click', () => playRound('paper'));
+document.getElementById('scissors').addEventListener('click', () => playRound('scissors'));
+
